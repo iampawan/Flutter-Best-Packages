36eeda24bfaf089642f5f3098aacae24c10f3ff6
Pawan Kumar <pawan221b@gmail.com>
2019-12-09 16:41:53 +0530

    commit done

--- lib/library/flutter_spinkit_example.dart
+++ lib/library/flutter_spinkit_example.dart
@@ -12,7 +12,7 @@ class _FlutterSpinkitExampleState extends State<FlutterSpinkitExample>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter Spinkit"),
+        title: Text("Flutter Spinkit"), 
       ),
       body: GridView.count(
         crossAxisCount: 5,
